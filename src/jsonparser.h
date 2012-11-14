53c81349e54426c660f441cdff98f6389a6bd20c
rmterra <r.mirandaterra@gmail.com>
2012-11-14 02:04:58 -0200

    Revision of references documentation for the classes

--- src/jsonparser.h
+++ src/jsonparser.h
@@ -1,3 +1,30 @@
+/*
+    This file is part of the GuaxinimJs project.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+        * Redistributions of source code must retain the above copyright
+          notice, this list of conditions and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright
+          notice, this list of conditions and the following disclaimer in the
+          documentation and/or other materials provided with the distribution.
+        * Neither the name of the <organization> nor the
+          names of its contributors may be used to endorse or promote products
+          derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #ifndef JSONPARSER_H
 #define JSONPARSER_H
 
@@ -5,14 +32,37 @@
 #include <QObject>
 #include <QScriptValue>
 
+/**
+  * A bunch of useful static functions to convert from QScriptValue Json based objects to another types
+  * This class shouldn't be instantiated
+  */
 class JSONParser : public QObject
 {
     Q_OBJECT
 
-public:
-    static QString toString(QScriptValue);
-    static QMap<QString, QString> toMap(QScriptValue);
+private:
+    JSONParser();
 
+public:
+    /**
+      * Convert the QScriptValue Json based on a literal string
+      *
+      * For instance the return will be:
+      * { foo: 'Hello World', bar: { value: 10 }, vect: [0,1,2] }
+      *
+      * @brief toString
+      * @param json - Json based object to convert
+      * @return QString that represent the literal json object
+      */
+    static QString toString(QScriptValue json);
+    /**
+      * Convert the QScriptValue Json based on a literal string
+      *
+      * @brief toString
+      * @param json - Json based object to convert
+      * @return QString that represent the literal json object
+      */
+    static QMap<QString, QString> toMap(QScriptValue json);
 };
 
 #endif // JSONPARSER_H
--- src/jsws.h
+++ src/jsws.h
@@ -1,3 +1,30 @@
+/*
+    This file is part of the GuaxinimJs project.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+        * Redistributions of source code must retain the above copyright
+          notice, this list of conditions and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright
+          notice, this list of conditions and the following disclaimer in the
+          documentation and/or other materials provided with the distribution.
+        * Neither the name of the <organization> nor the
+          names of its contributors may be used to endorse or promote products
+          derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #ifndef JSWS_H
 #define JSWS_H
 
@@ -12,6 +39,9 @@
 #include <src/requestparams.h>
 #include <src/webpage.h>
 
+/**
+  * The heart of GuaxinimJs, this class represent the main namespace of the project.
+  */
 class Jsws : public QObject
 {
     Q_OBJECT
@@ -25,11 +55,38 @@ public:
     explicit Jsws(QScriptEngine*, QString, QObject *parent = 0);
 
 public:
-    void async(bool) ;
+    /**
+      * Set if the default scrape proccess is async or not
+      *
+      * @todo Implement the use of this property
+      * @brief async
+      * @param value - new value
+      */
+    void async(bool value) ;
+    /**
+      * Set if the default scrape proccess is async or not
+      *
+      * @todo Implement the use of this property
+      * @brief isAsync
+      * @return Boolean true if the scrape proccess is async
+      */
     bool isAsync() const;
-
+    /**
+      * Recover the list of arguments used to run the script.
+      * The list is expected in this format:
+      * [ pathToApp, optionsUsedToRun, scriptFileName, otherArgs... ]
+      *
+      * @brief args
+      * @return String List with args passed on line command when the script startup
+      */
     QStringList args() const;
-
+    /**
+      * The page used for output of engine
+      *
+      * @see WebPage class for more details
+      * @brief page
+      * @return QscriptValue based on QWebPage class
+      */
     QScriptValue page() const;
 
 private:
@@ -51,8 +108,22 @@ private:
     void proccessSync();
 
 public slots:
-    void scrape(QString, QScriptValue);
-    void scrape(QStringList, QScriptValue);
+    /**
+      * Start the web scrape/crawling proccess based on a url
+      *
+      * @brief scrape
+      * @param url
+      * @param options
+      */
+    void scrape(QString url, QScriptValue options);
+    /**
+      * Start the web scrape/crawling proccess based on a list of url
+      *
+      * @brief scrape
+      * @param urls
+      * @param options
+      */
+    void scrape(QStringList ulrs, QScriptValue options);
     void request(QScriptValue);
 
     void toPdf(QString);
